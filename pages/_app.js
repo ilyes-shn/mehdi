f60c2e19fb0c5976bcfc1f9f09ba1733c9496ff3
ilyes-shn <infinityilyes@gmail.com>
2021-08-30 13:48:22 +0100

    social

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
 import "../styles/globals.css";
 import Head from "next/head";
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Konfigg</title>
+        <title>
+          Konfigg | Create and deploy new pages of your website in a few clicks
+        </title>
+        <meta charSet="UTF-8" />
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta
+          name="description"
+          content="Empower every visitor with unique content based on his keyword research."
+        />
+        <meta
+          property="og:title"
+          content="Konfigg | Create and deploy new pages of your website in a few clicks"
+        />
+        <meta
+          property="og:description"
+          content="Empower every visitor with unique content based on his keyword research."
+        />
+        <meta property="og:image" content="/social.png" />
+        <meta property="og:url" content="https://www.konfigg.com/" />
         <link rel="icon" href="/favicon.svg" type="image/svg" />
       </Head>
       <Component {...pageProps} />
